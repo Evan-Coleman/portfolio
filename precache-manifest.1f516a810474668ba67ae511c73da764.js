3005a6c58d0bc6a92c8b982d804ff0d4db42ca4f
Evan Coleman <ecoleman2@gmail.com>
2019-04-11 18:05:23 -0600

    Updates

--- precache-manifest.1f516a810474668ba67ae511c73da764.js
+++ precache-manifest.1f516a810474668ba67ae511c73da764.js
@@ -4,27 +4,27 @@ self.__precacheManifest = [
     "url": "/static/media/profile_picture.f759c72a.jpg"
   },
   {
-    "revision": "4f06dde486eba5a255691b9f2e6bf992",
-    "url": "/static/media/Evan_Coleman_Resume.4f06dde4.jpg"
+    "revision": "534c357f037d20f611cc886bc2f99aa1",
+    "url": "/static/media/Evan_Coleman_Resume.534c357f.jpg"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "c986d3cbe93c43f2e9d1",
-    "url": "/static/js/main.68f7404d.chunk.js"
+    "revision": "8ec0df6f6082b59bac96",
+    "url": "/static/js/main.3166142c.chunk.js"
   },
   {
     "revision": "357c0ecb9fb06f7b613b",
     "url": "/static/js/2.434c7551.chunk.js"
   },
   {
-    "revision": "c986d3cbe93c43f2e9d1",
+    "revision": "8ec0df6f6082b59bac96",
     "url": "/static/css/main.67abe85f.chunk.css"
   },
   {
-    "revision": "919b06a40fbb8674ce87fde5af85c2a7",
+    "revision": "256dcf9d1483a7f18556cc2adf9d2153",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cace6f9e7387ea75fef68ce405d07973.js"
+  "/precache-manifest.1f516a810474668ba67ae511c73da764.js"
 );
 
 workbox.clientsClaim();
